eac8ac162e0f9d9caa7b1684dda5544519937f5b
lord asado <granlordasado@gmail.com>
2025-10-01 00:20:27 -0300

    Add new colorschemes

--- init.lua
+++ init.lua
@@ -885,16 +885,31 @@ require('lazy').setup({
     },
   },
 
+  {
+    'folke/tokyonight.nvim',
+    priority = 1000,
+  },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+  },
+  {
+    'olivercederborg/poimandres.nvim',
+    lazy = false,
+    priority = 1000,
+  },
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'bjarneo/pixel.nvim',
+    name = 'pixel',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
+      ---diagnostic disable-next-line: missing-fields
+      require('pixel').setup {
         styles = {
           comments = { italic = false }, -- Disable italics in comments
         },
@@ -903,7 +918,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'catppuccin'
     end,
   },
 
