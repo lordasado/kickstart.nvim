6601271c3db12a4b905a656d4d37600a418e8192
lord asado <granlordasado@gmail.com>
2025-10-18 10:42:59 -0300

    feat: add dadbod viewer

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -181,18 +181,20 @@ return {
     end,
   },
   {
-    'kndndrj/nvim-dbee',
+    'kristijanhusak/vim-dadbod-ui',
     dependencies = {
-      'MunifTanjim/nui.nvim',
+      { 'tpope/vim-dadbod', lazy = true },
+      { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }, -- Optional
     },
-    build = function()
-      -- Install tries to automatically detect the install method.
-      -- if it fails, try calling it with one of these parameters:
-      --    "curl", "wget", "bitsadmin", "go"
-      require('dbee').install()
-    end,
-    config = function()
-      require('dbee').setup(--[[optional config]])
+    cmd = {
+      'DBUI',
+      'DBUIToggle',
+      'DBUIAddConnection',
+      'DBUIFindBuffer',
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
     end,
   },
 }
