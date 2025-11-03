b3415bcd31634069729222f1cd472f72dee4b684
lord asado <granlordasado@gmail.com>
2025-11-02 21:45:06 -0300

    feat: add image and brainrot plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -197,4 +197,42 @@ return {
       vim.g.db_ui_use_nerd_fonts = 1
     end,
   },
+  {
+    '3rd/image.nvim',
+    build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+    opts = {
+      processor = 'magick_cli',
+      max_width = 800,
+      integrations = {
+        markdown = {
+          enabled = false,
+        },
+      },
+    },
+  },
+  {
+    'sahaj-b/brainrot.nvim',
+    event = 'VeryLazy',
+    opts = {
+      -- defaults:
+
+      disable_phonk = false, -- skip phonk/overlay on "no errors"
+      phonk_time = 1.0, -- seconds the phonk/image overlay stays
+      min_error_duration = 0.5, -- minimum seconds errors must exist before phonk triggers (0 = instant)
+      block_input = false, -- block input during phonk/overlay
+      dim_level = 20, -- phonk overlay darkness 0..100
+
+      sound_enabled = true, -- enable sounds
+      image_enabled = true, -- enable images (needs image.nvim)
+
+      boom_volume = 50, -- volume for vine boom sound (0..100)
+      phonk_volume = 20, -- volume for phonk sound (0..100)
+
+      boom_sound = nil, -- custom boom sound path (e.g., "~/sounds/boom.ogg")
+      phonk_dir = nil, -- custom phonk folder path (e.g., "~/sounds/phonks")
+      image_dir = nil, -- custom image folder path (e.g., "~/memes/images")
+
+      lsp_wide = false, -- track errors workspace-wide(get ALL lsp errors)
+    },
+  },
 }
