d035ebb564ce5522f11f3f718d75ed3df5a5da15
lord asado <granlordasado@gmail.com>
2025-12-10 19:17:57 -0300

    feat(nvim-origami): add code folds module

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -330,12 +330,48 @@ return {
       foldKeymaps = {
         setup = false, -- modifies `h`, `l`, `^`, and `$`
       },
+      foldtext = {
+        lineCount = {
+          template = ' %d',
+        },
+      },
     }, -- needed even when using default config
 
     -- recommended: disable vim's auto-folding
     init = function()
       vim.opt.foldlevel = 99
       vim.opt.foldlevelstart = 99
+
+      local fold_util = require 'utils.code_folds'
+
+      vim.api.nvim_create_autocmd({ 'TextChanged', 'InsertLeave', 'LspAttach' }, {
+        callback = function(opts)
+          fold_util.update_ranges(opts.buf)
+        end,
+      })
+
+      local last_row = nil
+      vim.api.nvim_create_autocmd('CursorMoved', {
+        callback = function(opts)
+          local row = vim.api.nvim_win_get_cursor(0)[1]
+          if row ~= last_row then
+            last_row = row
+
+            fold_util.update_current_fold(row, opts.buf)
+          end
+        end,
+      })
+
+      vim.api.nvim_create_autocmd({ 'BufUnload', 'BufWipeout' }, {
+        callback = function(opts)
+          fold_util.clear(opts.buf)
+        end,
+      })
+
+      vim.opt.statuscolumn = '%!v:lua.StatusCol()'
+      function _G.StatusCol()
+        return fold_util.statuscol()
+      end
     end,
   },
 }
