abea2fa8329009cb525a165d2c08b6030cc6cea4
lord asado <granlordasado@gmail.com>
2025-12-10 20:40:25 -0300

    feat: add aerial.nvim component to lualine

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -7,7 +7,40 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-      require('lualine').setup {}
+      require('lualine').setup {
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_c = { 'filetype', 'filename' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' },
+
+          -- Or you can customize it
+          lualine_x = {
+            {
+              'aerial',
+              -- The separator to be used to separate symbols in status line.
+              sep = ' ) ',
+
+              -- The number of symbols to render top-down. In order to render only 'N' last
+              -- symbols, negative numbers may be supplied. For instance, 'depth = -1' can
+              -- be used in order to render only current symbol.
+              depth = nil,
+
+              -- When 'dense' mode is on, icons are not rendered near their symbols. Only
+              -- a single icon that represents the kind of current symbol is rendered at
+              -- the beginning of status line.
+              dense = false,
+
+              -- The separator to be used to separate symbols in dense mode.
+              dense_sep = '.',
+
+              -- Color the symbol icons.
+              colored = true,
+            },
+          },
+        },
+      }
     end,
   },
   {
