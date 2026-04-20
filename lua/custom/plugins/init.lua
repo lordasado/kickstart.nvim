1a4f06b66ed307283cefe6f9d61593587237ef2a
lord asado <granlordasado@gmail.com>
2026-04-19 17:50:53 -0700

    feat: suppress deprecation

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -303,6 +303,7 @@ return {
         tailwind_opts = { -- Options for highlighting tailwind names
           update_names = true, -- When using tailwind = 'both', update tailwind names from LSP results.  See tailwind section
         },
+        suppress_deprecation = true,
       },
     },
   },
