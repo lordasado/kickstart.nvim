a36a7201a2cd1cf68a5d387f853f740fe87294c7
lord asado <granlordasado@gmail.com>
2026-04-26 03:45:55 -0700

    feat: add nvim-lint

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -402,4 +402,31 @@ return {
       end,
     },
   },
+  {
+    'mfussenegger/nvim-lint',
+    event = {
+      'BufReadPre',
+      'BufNewFile',
+    },
+    config = function()
+      local lint = require 'lint'
+      lint.linters_by_ft = {
+        javascript = { 'oxlint' },
+        javascriptreact = { 'oxlint' },
+        typescript = { 'oxlint' },
+        typescriptreact = { 'oxlint' },
+        svelte = { 'oxlint' },
+        astro = { 'oxlint' },
+      }
+
+      local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'TextChanged' }, {
+        group = lint_augroup,
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+    end,
+  },
 }
