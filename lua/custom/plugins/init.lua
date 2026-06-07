a2cc853c6526deb0165062672f47545d8529dfbf
lord asado <granlordasado@gmail.com>
2026-06-07 16:48:14 -0700

    feat: remove token-count plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -38,7 +38,7 @@ return {
               colored = true,
             },
           },
-          lualine_y = { 'progress', require('token-count.integrations.lualine').current_buffer },
+          lualine_y = { 'progress' },
           lualine_z = { 'location' },
         },
       }
@@ -387,20 +387,6 @@ return {
       vim.keymap.set('n', '<leader>af', '<cmd>AerialToggle float<CR>', { desc = 'Aerial: Toggle [f]loat' })
       vim.keymap.set('n', '<leader>ae', '<cmd>AerialToggle!<CR>', { desc = 'Aerial: Toggle Sid[e]bar' })
     end,
-    {
-      '3ZsForInsomnia/token-count.nvim',
-      opts = {
-        model = 'gpt-5', -- Default model for counting
-      },
-      config = function()
-        require('token-count').setup {
-          vim.keymap.set('v', '<leader>tc', ':TokenCountSelection<CR>', {
-            desc = '[C]ount tokens in visual selection',
-            silent = true,
-          }),
-        }
-      end,
-    },
   },
   {
     'mfussenegger/nvim-lint',
