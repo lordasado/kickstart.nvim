bb6558286fc86d4abe19f4b34ae8e5ff0dfd17f2
lord asado <granlordasado@gmail.com>
2026-06-16 05:05:29 -0700

    feat: delete rustowl

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -429,10 +429,5 @@ return {
     lazy = false,
   },
   {
-    'cordx56/rustowl',
-    version = '*', -- Latest stable version
-    build = 'cargo install rustowl',
-    lazy = false, -- This plugin is already lazy
-    opts = {},
   },
 }
