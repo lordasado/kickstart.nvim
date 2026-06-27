70d336fb7cb9854b778631a09472ca060dea72be
lord asado <granlordasado@gmail.com>
2026-06-26 23:52:35 -0700

    feat: add obsidian.nvim plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -511,4 +511,26 @@ return {
       }
     end,
   },
+  {
+    'obsidian-nvim/obsidian.nvim',
+    version = '*', -- use latest release, remove to use latest commit
+    ---@module 'obsidian'
+    ---@type obsidian.config
+    opts = {
+      legacy_commands = false, -- this will be removed in 4.0.0
+      workspaces = {
+        {
+          name = 'Jeff OS',
+          path = '/Users/lord/Documents/Obsidian/Jeff OS',
+        },
+      },
+    },
+    -- config = function()
+    require('telescope').setup {
+      picker = {
+        name = 'telescope.nvim', -- or telescope
+      },
+    },
+    -- end,
+  },
 }
