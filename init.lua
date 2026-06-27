ad6df025c264c44047f510a22a727106d5bd34d2
lord asado <granlordasado@gmail.com>
2026-06-26 23:53:33 -0700

    feat(blink.cmp): enable ghost text

--- init.lua
+++ init.lua
@@ -906,7 +906,7 @@ require('lazy').setup({
         -- By default, you may press `<c-space>` to show the documentation.
         -- Optionally, set `auto_show = true` to show the documentation after a delay.
         documentation = { auto_show = true, auto_show_delay_ms = 500 },
-        -- ghost_text = { enabled = false },
+        ghost_text = { enabled = true },
       },
 
       sources = {
